fac5cc0c01569e4e50a080c20b064004ecded8dc
usman-nc <97964408+usman-nc@users.noreply.github.com>
2022-01-18 16:08:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 8080
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run from Usman')
+    res.end('Hello Cloud Run from Masood')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
